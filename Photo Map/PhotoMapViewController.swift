4166b0b250ee904e44aabddf7d9a21e9d3fd96e6
richardmatthias <richardmatthias@126.com>
2018-03-07 21:28:26 -0800

    Finished

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,99 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class PhotoMapViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate,LocationsViewControllerDelegate,MKMapViewDelegate {
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        lat=latitude
+        lng=longitude
+        let locationCoordinates=CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoordinates
+        annotation.title = "Picture!"
+        mapView.addAnnotation(annotation)
+        mapView.delegate=self
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+    @IBOutlet weak var mapView: MKMapView!
+    var takenImage:UIImage!
+    var lat:NSNumber!
+    var lng:NSNumber!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func photoTap(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        takenImage=editedImage;
+        // Do something with the images (based on your use case)
+        performSegue(withIdentifier: "tagSegue", sender: self)
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = UIImage(named: "camera")
+        
+        return annotationView
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        let desti=segue.destination as? LocationsViewController
+        desti?.delegate=self
+    }
+    
+
+}
